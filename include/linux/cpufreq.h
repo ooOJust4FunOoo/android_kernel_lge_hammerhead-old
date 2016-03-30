e37f95320d18ab0f1419dd4194cf80e7ab4b3bc7
Steeve Zeiger <steeve.zeiger@gmail.com>
2016-03-30 04:00:57 +0200

    Add zzmoove, yankactive and yankdemand governor
    ZZMoove description:
    'ZZMoove' governor is based on the modified 'conservative'
    (original author Alexander Clouter <alex@digriz.org.uk>)
    'smoove' governor from Michael Weingaertner <mialwe@googlemail.com>
    (source: https://github.com/mialwe/mngb/)
    ported/modified/optimzed for I9300 since November 2012 and further
    improved for exynos and snapdragon platform (but also working on other
    platforms like OMAP) by ZaneZam,Yank555 and ffolkes in 2013/14/15
    CPU Hotplug modifications partially taken from ktoonservative governor
    from ktoonsez KT747-JB kernel (https://github.com/ktoonsez/KT747-JB)
    
    Yankactive description:
    Interactive governor with, fow now :
    - hispeed_freq defaulting to lower than policy_max (1.73GHz)
    - target_loads increased to 95
    - min_sampling time reduced to 20ms
    - above_hispeed_delay increased to 80ms
    - timer_rate increased to 20ms
    cpufreq: yankactive: Optimize sync_freq implementation
    Don't let frequency drop below sync_freq if either load or
    frequency among other cpus is above a certain threshold,
    not necessarily both.
    Credits : cyanogen
    
    Yankdemand description:
    Full stock (JB) ondemand governor with changed default
    tunable values aimed at lower battery consumption
    
    zzmoove from:
    https://github.com/zanezam/cpufreq-governor-zzmoove
    
    yankactive and yankdemand from:
    https://github.com/yank555-lu/Hammerhead-3.4-lollipop

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -444,7 +444,16 @@ extern struct cpufreq_governor cpufreq_gov_optimax;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SLIM)
 extern struct cpufreq_governor cpufreq_gov_slim;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_slim)
-#endif
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_YANKACTIVE)
+ extern struct cpufreq_governor cpufreq_gov_yankactive;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_yankactive)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_YANKDEMAND)
+extern struct cpufreq_governor cpufreq_gov_yankdemand;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_yankdemand)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ZZMOOVE)
+extern struct cpufreq_governor cpufreq_gov_zzmoove;
+#define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_zzmoove)
+ #endif
 
 
 /*********************************************************************
